dd634374214b78e929ea1eae040bc9e0055db5b3
srija-neu <pallavarapu.s@northeastern.edu>
2025-10-17 19:52:39 -0400

    Member Greeting file

---
+++ MemberGreeting/src/Model/Member2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Srija
+ */
+public class Member2 {
+    public static void main(String[] args) {
+        System.out.println("Hello from Srija!!");
+        System.out.println("Member greetings for Group Lab!");
+}
+}
