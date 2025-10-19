ab0a9cb522d9749871a2a06878f4c74578deb586
vaishrao2601 <srinivas.vai@northeastern.edu>
2025-10-19 15:32:35 -0400

    Added greeting for member 1

---
+++ MemberGreeting/src/Model/Member1.java
@@ -0,0 +1,18 @@
+package Model;
+
+/**
+ *
+ * @author vaishu
+ */
+public class Member1 {
+
+    public static void main(String[] args) {
+        String studentName = "Vaishnavi Srinivas";   
+        String role = "Member 1 (Group 20)";      
+
+        System.out.println("Hello from " + studentName + " — Role: " + role);
+        System.out.println("INFO5100 University Example is ready.");
+    
+    }
+    
+}
